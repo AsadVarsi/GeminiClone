7846c1e92add3116f28de12b031041ac7166a1fa
asadvarsi <asdvarsi@gmail.com>
2025-03-26 11:52:24 +0530

    geminiClone

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,86 @@
+/* eslint-disable no-unused-vars */
+import React, { useContext } from 'react'
+import "./Main.css"
+import { assets } from '../../assets/assets'
+import { Context } from '../../context/Context'
+
+function Main() {
+
+    const {onSent,recentPrompt,showResult,loading,resultData,setInput,input} = useContext(Context)
+    console.log(recentPrompt+"hellp");
+  return (
+    <div className='main'>
+        <div className="nav">
+            <p>Gemini</p>
+            <img src={assets.gemini_icon} alt="" />
+        </div>
+        {!showResult?
+        <>
+        <div className="main-container">
+            <div className="greet">
+                <p><span>Hello, Dev.</span></p>
+                <p>How con I help you today?</p>
+            </div>
+            <div className="cards">
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.compass_icon} alt="" />
+                </div>
+
+                <div className="card">
+                    <p>Briefly summrize this concept:urban Planning</p>
+                    <img src={assets.bulb_icon} alt="" />
+                </div>
+
+                <div className="card">
+                    <p>Brainstrome team bonding activities for our work retreat</p>
+                    <img src={assets.message_icon} alt="" />
+                </div>
+
+                <div className="card">
+                    <p>Suggest beautiful places to see on an upcoming road trip</p>
+                    <img src={assets.code_icon} alt="" />
+                </div>
+            </div>
+        </div>
+        </>:
+        <div className='result'>
+            <div className="result-title">
+                <img src={assets.user_icon} alt="" />
+                <p>{recentPrompt}</p>
+            </div>
+            <div className="result-data">
+                <img src={assets.gemini_icon} alt="" />
+                {loading?
+                <>
+                <div className="loader">
+                    <hr />
+                    <hr />
+                    <hr />
+                </div>
+                </>:
+                <p dangerouslySetInnerHTML={{__html:resultData}}></p>
+                }
+            </div>
+
+        </div>  
+        }     
+        <div className="main-bottom">
+            <div className="search-box">
+                <input type="text" onChange={(e)=>{setInput(e.target.value)}} placeholder='Ask Gemini' />
+                <div>
+                    <img src={assets.mic_icon} alt="" />
+                    <img src={assets.gallery_icon} alt="" />
+                    <img src={assets.send_icon} onClick={(e)=>{onSent()}} alt="" />
+                </div>
+            </div>
+            <p className='bottom-info'>
+                Gemini may display wrong content use it gracefully 
+            </p>
+        </div>
+
+    </div>
+  )
+}
+
+export default Main
